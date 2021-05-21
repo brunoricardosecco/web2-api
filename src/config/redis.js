24fe01394ea481f3e74f097ff9a3748ed29c6d0f
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2021-05-20 23:30:05 -0300

    fix: add redis password

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,6 +6,7 @@ const redis = require('express-redis-cache');
 const cache = redis({
   port: process.env.REDIS_PORT,
   host: process.env.REDIS_HOST,
+  auth_pass: process.env.REDIS_PASSWORD,
 });
 
 cache.invalidate = (name) => (req, res, next) => {
